34aa2c376a0b5d93b30f3f73e15a757986d6413b
Erhan Karakuyu <erhan.k123@gmail.com>
2023-02-23 16:38:46 +1100

    feat: Updated CSS for the hero section

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 const active = document.querySelector(".hamburger-img-active");
 const inactive = document.querySelector(".hamburger-img-inactive");
-const menu = document.querySelector(".hamburger-ul");
+const menu = document.querySelector(".hamburger-nav");
 const main = document.querySelector("body");
+const image = document.querySelector(".hero-mobile-photo");
+const menuLinks = document.querySelectorAll(".hamburger-ul li a");
 
 active.addEventListener("click", () => {
   menu.classList.toggle("show");
+  image.classList.toggle("show");
+  main.classList.toggle("show");
+  menuLinks.forEach((menu) => {
+    menu.classList.toggle("show");
+  });
 });
 
 inactive.addEventListener("click", () => {
   menu.classList.toggle("show");
-});
-
-main.addEventListener("click", (event) => {
-  const current = event.currentTarget;
-  current.classList.toggle("show");
+  image.classList.toggle("show");
+  main.classList.toggle("show");
+  menuLinks.forEach((menu) => {
+    menu.classList.toggle("show");
+  });
 });
